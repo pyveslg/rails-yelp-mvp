c0177f4f22993438cbe7977bcfdcb28eb85a12ff
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-05-17 12:31:55 +0200

    Rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "italian"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "french"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "french"
+
+  },
+  {
+    name:         "Le Chateaubriand",
+    address:      "129 avenue Parmentier 75011 Paris",
+    category:     "french"
+  },
+  {
+    name:         "Pane Olio e Pomodoro",
+    address:      "46 rue de la Fontaine au Roi 75011 Paris",
+    category:     "italian"
+  }
+
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
